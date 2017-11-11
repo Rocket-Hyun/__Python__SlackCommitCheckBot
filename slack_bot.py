fe200d6beff920606b2dac26cf8f9622fb45b15e
Rocket-Hyun <rockethyun@gmail.com>
2017-11-12 03:38:39 +0900

    added star

--- slack_bot.py
+++ slack_bot.py
@@ -11,7 +11,7 @@ korean_time = datetime.now() + timedelta(hours=9)
 textfile = open("github_repositories.txt", "r", encoding="utf8")
 members_github = textfile.read()
 members_github_list = members_github.split(",")
-members_commit_check = [korean_time.strftime("%Y-%m-%d")+"의 커밋 결과\n"]
+members_commit_check = ["★★★"+korean_time.strftime("%Y-%m-%d")+"의 커밋 결과★★★\n"]
 for url in members_github_list:
     if gp.commit_checker(url):
         result = "완료"
